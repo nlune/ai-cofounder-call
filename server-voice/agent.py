21504e58f98206da8f718a2570b90ca264d280a9
Lune <phenixlune@gmail.com>
2025-01-19 13:34:42 +0100

    working demo

---
+++ server-voice/agent.py
@@ -0,0 +1,168 @@
+import logging
+from groq import Groq
+import os
+
+from dotenv import load_dotenv
+from livekit.agents import (
+    AutoSubscribe,
+    JobContext,
+    JobProcess,
+    WorkerOptions,
+    cli,
+    llm,
+)
+from livekit.agents.pipeline import VoicePipelineAgent
+from livekit.plugins import openai, deepgram, silero
+from livekit.plugins.openai import stt
+
+from livekit.agents import llm
+from typing import Annotated
+import json
+
+
+class AssistantFnc(llm.FunctionContext):
+    def __init__(self, room=None, participant=None):
+        super().__init__()
+        self.room = room
+        self.participant = participant
+        
+    @llm.ai_callable()
+    async def agent_task(self, task:  Annotated[str, llm.TypeInfo(description="task to add to the board for AI agent")]):
+        """
+        Process the last user message and generate a corresponding task.
+        Args:
+            task: task to add to the board for AI cofounder
+        """
+        
+        # participant = await ctx.wait_for_participant()
+        logger.info("agent_task function was called!")
+
+        if task:
+            result = task
+            try:
+                logger.info("perform rtc")
+                return await self.room.local_participant.perform_rpc(
+                destination_identity= self.participant.identity,
+                method="getAgentTask",
+                payload=json.dumps({
+                    "task": result,
+                    "name": 'AI CoFounder'
+                }),
+                response_timeout=5.0,
+                )
+
+            except Exception as e:
+                logger.warning(f"errorrrr {e}")
+                return f"Error sending task result to frontend: {e}"
+
+    @llm.ai_callable()
+    async def human_task(self, task:  Annotated[str, llm.TypeInfo(description="task to add to the board for human")]):
+        """
+        Process the last user message and generate a corresponding task.
+        Args:
+            task: task to add to the board for cofounder
+        """
+        
+        # participant = await ctx.wait_for_participant()
+        logger.info("human_task function was called!")
+
+        if task:
+            result = task
+            try:
+                logger.info("perform rtc")
+                return await self.room.local_participant.perform_rpc(
+                destination_identity= self.participant.identity,
+                method="getAgentTask",
+                payload=json.dumps({
+                    "task": result,
+                    "name": 'Lune'
+                }),
+                response_timeout=5.0,
+                )
+
+            except Exception as e:
+                logger.warning(f"errorrrr {e}")
+                return f"Error sending task result to frontend: {e}"
+
+load_dotenv(dotenv_path=".env.local")
+logger = logging.getLogger("voice-agent")
+client = Groq(
+        api_key=os.environ.get("GROQ_API_KEY"),
+    )
+
+
+def prewarm(proc: JobProcess):
+    proc.userdata["vad"] = silero.VAD.load()
+
+
+async def entrypoint(ctx: JobContext):
+    initial_ctx = llm.ChatContext().append(
+        role="system",
+        text=(
+            "You are an AI cofounder. Your interface with the user Lune will be via voice. "
+            "You will be able to understand and generate human-like speech, and be helpful in your responses. Talk in a natural human tone. Give insightful prompts where possible."
+            "You have access to a function called 'agent_task' and a function called 'human_task' that takes a 'task' argument."
+            "If the task if for yourself, use the 'agent_task' function."
+            "If the task is for the human cofounder, use the 'human_task' function."
+            "You must call the agent_task function using the following format:"
+            "<function>agent_task(task='...')</function>"
+            "You must call the human_task function using the following format:"
+            "<function>human_task(task='...')</function>"
+            "IMPORTANT: Do not use the function call with any other text. Process the user message to get the correct task to make sure to add tasks appropriately."
+        ),
+    )
+
+    logger.info(f"connecting to room {ctx.room.name}")
+    await ctx.connect(auto_subscribe=AutoSubscribe.AUDIO_ONLY)
+
+    # Wait for the first participant to connect
+    participant = await ctx.wait_for_participant()
+    logger.info(f"starting voice assistant for participant {participant.identity}")
+
+    # This project is configured to use Deepgram STT, OpenAI LLM and TTS plugins
+    # Other great providers exist like Cartesia and ElevenLabs
+    # Learn more and pick the best one for your app:
+    # https://docs.livekit.io/agents/plugins
+    
+    groq_stt = stt.STT.with_groq(
+    model="whisper-large-v3-turbo",
+    language="en",
+    )
+    
+    fnc_ctx = AssistantFnc(room=ctx.room, participant=participant)
+    
+    agent = VoicePipelineAgent(
+        vad=ctx.proc.userdata["vad"],
+        stt=groq_stt,
+        llm=openai.LLM(model="gpt-4o-mini"),
+        tts=openai.TTS(voice="fable", speed=1),
+        chat_ctx=initial_ctx,
+        fnc_ctx=fnc_ctx,
+    )
+
+    agent.start(ctx.room, participant)
+    
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "system",
+                "content": """You are an AI cofounder communicating via a live voice interface.
+                You will be able to understand and generate human-like speech, and be helpful in your responses.
+                Your cofounder just called you. Respond appropriately. Don't ramble. """,
+            },
+        ],
+        model="llama3-8b-8192",
+    )
+
+
+    # The agent should be polite and greet the user when it joins :)
+    await agent.say(chat_completion.choices[0].message.content, allow_interruptions=True)
+
+
+if __name__ == "__main__":
+    cli.run_app(
+        WorkerOptions(
+            entrypoint_fnc=entrypoint,
+            prewarm_fnc=prewarm,
+        ),
+    )
